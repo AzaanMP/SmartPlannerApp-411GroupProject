89b7e76419016153b60df883f3cb64585f39c42c
AzaanMP <azaanmp2@gmail.com>
2026-04-10 14:34:08 -0700

    Initial Commit

---
+++ SmartPlannerApp/SmartPlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPlannerAppApp.swift
+//  SmartPlannerApp
+//
+//  Created by csuftitan on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartPlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
