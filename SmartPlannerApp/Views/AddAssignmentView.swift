881fa5f45e4d7ddecc539da4a34dcce63c42f657
AzaanMP <azaanmp2@gmail.com>
2026-04-10 18:33:33 -0700

    Fixed SwiftUI Codes

--- SmartPlannerApp/Views/AddAssignmentView.swift
+++ SmartPlannerApp/Views/AddAssignmentView.swift
@@ -4,3 +4,15 @@
 //
 //  Created by csuftitan on 4/10/26.
 //
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAssignmentView()
+}
